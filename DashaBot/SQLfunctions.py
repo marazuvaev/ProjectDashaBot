b2205d0a48ee92063c2944c1b2c891142fbd77db
starRICK <marazuvaev@edu.hse.ru>
2024-12-02 19:26:04 +0300

    db 7

--- DashaBot/SQLfunctions.py
+++ DashaBot/SQLfunctions.py
@@ -5,16 +5,16 @@ def add_admin(user_id: int, chat_name: str):
     connection = sq.connect('chats.db')
     cursor = connection.cursor()
     cursor.execute("INSERT INTO admins (admin_id, chat_name) VALUES (?, ?)", (user_id, chat_name))
-    connection.close()
     cursor.close()
+    connection.close()
 
 
 def chat_cheker(user_id: int, chat_name: str):
     connection = sq.connect('chats.db')
     cursor = connection.cursor()
     a = cursor.execute("SELECT (admin_id, chat_name) WHERE admin_id = ? AND chat_name = ?", (user_id, chat_name))
-    connection.close()
     cursor.close()
+    connection.close()
     return len(a.fetchall()) != 0
 
 
@@ -22,8 +22,8 @@ def add_chat_to_db(chat_name: str, chat_id: int, admin_id: int):
     connection = sq.connect('chats.db')
     cursor = connection.cursor()
     cursor.execute("INSERT INTO chats (telegram_id, admin_id, chat_name) VALUES (?, ?, ?)", (chat_id, admin_id, chat_name))
-    connection.close()
     cursor.close()
+    connection.close()
 
 
 def add_members(chat_name: str, admin_id: int, members: str):
